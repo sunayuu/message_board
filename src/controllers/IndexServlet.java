4995358ba54ce80a35aafec541da79835aee6c99
yuuya.sunagawa <yuya.sunagawa@growship.com>
2022-04-20 14:11:55 +0900

    Index/NewServletの修正

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,11 +1,19 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
+
 
 /**
  * Servlet implementation class IndexServlet
@@ -13,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,9 +33,16 @@ public class IndexServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
 
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+
+        em.close();
+
+        request.setAttribute("messages", messages);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        rd.forward(request, response);
+    }
 }
